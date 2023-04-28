3d02908a72a9340fc8fbb9b71e92ced4608390eb
Gleb Rubashenko <braginez.gr61@gmail.com>
2023-04-28 22:21:34 +0300

    DI 2

--- src/main/java/by/rubashenko/springcourse/MusicPlayer.java
+++ src/main/java/by/rubashenko/springcourse/MusicPlayer.java
@@ -1,14 +1,43 @@
 package by.rubashenko.springcourse;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
 
     // IoC
-    public MusicPlayer(Music music) {
+/*    public MusicPlayer(Music music) {
         this.music = music;
+    }*/
+
+    public MusicPlayer() {}
+
+    public void setMusic(List<Music> musicList) {
+        this.musicList = musicList;
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void playMusicList() {
+        for (Music music : musicList) {
+            System.out.println("Playing: " + music.getSong());
+        }
     }
 }
---
+++ src/main/java/by/rubashenko/springcourse/RapMusic.java
@@ -0,0 +1,8 @@
+package by.rubashenko.springcourse;
+
+public class RapMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Rap God";
+    }
+}
