ba93d4d1ef94418e533bdf5a9b06a016615b1c46
Gleb Rubashenko <braginez.gr61@gmail.com>
2023-04-30 01:45:25 +0300

    @autowired

--- annotations/src/main/java/by/rubashenko/springcourse/TestSpring.java
+++ annotations/src/main/java/by/rubashenko/springcourse/TestSpring.java
@@ -8,7 +8,7 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("rockMusic", Music.class);
+/*        Music music = context.getBean("rockMusic", Music.class);
 
         MusicPlayer musicPlayer = new MusicPlayer(music);
         musicPlayer.playMusic();
@@ -16,7 +16,15 @@ public class TestSpring {
         Music music2 = context.getBean("classicalMusic", Music.class);
 
         MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
-        classicalMusicPlayer.playMusic();
+        classicalMusicPlayer.playMusic();*/
+
+
+/*        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        musicPlayer.playMusic();*/
+
+
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
