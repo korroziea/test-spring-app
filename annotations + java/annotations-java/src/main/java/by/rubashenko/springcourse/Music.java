9d6b567ce43e11cbe913f43f963a3c195f1e88f8
Gleb Rubashenko <braginez.gr61@gmail.com>
2023-05-01 03:18:11 +0300

    @ Configuration, @ Bean

---
+++ annotations + java/annotations-java/src/main/java/by/rubashenko/springcourse/Music.java
@@ -0,0 +1,7 @@
+package by.rubashenko.springcourse;
+
+import java.util.List;
+
+public interface Music {
+    List<String> getSongs();
+}
---
+++ annotations + java/annotations-java/src/main/java/by/rubashenko/springcourse/MusicPlayer.java
@@ -0,0 +1,41 @@
+package by.rubashenko.springcourse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Random;
+
+public class MusicPlayer {
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+    private Music music1;
+    private Music music2;
+
+    public MusicPlayer(@Qualifier("rockMusic") Music music1,
+                       @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
+    }
+
+    public void playMusic(Enum genre) {
+        int randomSongNumber = new Random().nextInt(3);
+
+        if (genre == Genre.ROCK) {
+            System.out.println(music1.getSongs().get(randomSongNumber));
+        } else {
+            System.out.println(music2.getSongs().get(randomSongNumber));
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+}
