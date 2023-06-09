6a89f1031839fc142ca2f04163b2f69b04a40251
Gleb Rubashenko <braginez.gr61@gmail.com>
2023-06-10 01:14:35 +0300

    made function calculator

--- SpringMVCApp1/src/main/java/by/rubashenko/springcourse/controllers/FirstController.java
+++ SpringMVCApp1/src/main/java/by/rubashenko/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package by.rubashenko.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework  .web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,9 +14,11 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        // System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
     }
@@ -23,5 +26,40 @@ public class FirstController {
     @GetMapping("/goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam(value = "a", required = false) int a,
+                            @RequestParam(value = "b", required = false) int b,
+                            @RequestParam(value = "action", required = false) String action,
+                            Model model) {
+
+        double result;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+
+            case "addiction":
+                result = a + b;
+                break;
+
+            case "subtraction":
+                result = a - b;
+                break;
+
+            case "division":
+                result = a / (double) b;
+                break;
+
+            default:
+                result = 0;
+                break;
+        }
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 }
