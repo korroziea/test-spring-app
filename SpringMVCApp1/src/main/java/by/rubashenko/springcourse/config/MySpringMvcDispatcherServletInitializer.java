48dcdd4f780114add63cdc2f19ac5986cc2a261b
Gleb Rubashenko <braginez.gr61@gmail.com>
2023-06-09 14:20:44 +0300

    added new controller and new view

--- SpringMVCApp1/src/main/java/by/rubashenko/springcourse/config/MySpringMvcDispatcherServletInitializer.java
+++ SpringMVCApp1/src/main/java/by/rubashenko/springcourse/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,10 +1,8 @@
 package by.rubashenko.springcourse.config;
 
-//spring_mvc_app_java_config_war_exploded/
-
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class MySpringMvcDispatcherSerlvetInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return null;
